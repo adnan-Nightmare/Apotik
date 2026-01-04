5707d3a4d4c1589143803814560c4765dd4d6c4f
Muhammad Adnan <114914685+adnan-Nightmare@users.noreply.github.com>
2026-01-04 13:22:52 +0800

    Update StockTotal.php

--- app/Models/StockTotal.php
+++ app/Models/StockTotal.php
@@ -1,15 +1,1 @@
-<?php
 
-namespace App\Models;
-
-use Illuminate\Database\Eloquent\Model;
-
-class StockTotal extends Model
-{
-    protected $guarded = [];
-
-    public function medicine()
-    {
-        return $this->belongsTo(medicines::class);
-    }
-}
