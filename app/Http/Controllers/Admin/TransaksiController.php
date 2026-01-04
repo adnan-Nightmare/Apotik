9c7554fdcc3cd6ecf94ba2b8d34e7fcfbb697cf5
Muhammad Adnan <114914685+adnan-Nightmare@users.noreply.github.com>
2026-01-04 13:18:51 +0800

    Remove unused StockTotal model import

--- app/Http/Controllers/Admin/TransaksiController.php
+++ app/Http/Controllers/Admin/TransaksiController.php
@@ -7,7 +7,6 @@ use App\Models\Cart;
 use App\Models\customer;
 use App\Models\kategori;
 use App\Models\medicines;
-use App\Models\StockTotal;
 use App\Models\Transaction;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
